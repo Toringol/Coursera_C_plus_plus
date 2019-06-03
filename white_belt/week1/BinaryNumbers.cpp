3365e4424d411dbd7e460f4edba9654353383964
Toringol <37870973+Toringol@users.noreply.github.com>
2019-06-03 17:00:42 +0300

    Create BinaryNumbers.cpp

---
+++ white_belt/week1/BinaryNumbers.cpp
@@ -0,0 +1,24 @@
+/*
+* На вход дано целое положительное число N. 
+* Выведите его в двоичной системе счисления без ведущих нулей.
+*/
+
+#include <iostream>
+#include <vector>
+
+int main() {
+	int n;
+	std::cin >> n;
+
+	std::vector<int> binary;
+
+	while (n > 0) {
+		binary.push_back(n % 2);
+		n /= 2;
+	}
+
+	for (int i = binary.size() - 1; i >= 0; --i)
+		std::cout << binary[i];
+
+	return 0;
+}
