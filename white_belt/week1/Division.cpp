594d2384be3fdba7e8871461f68d09c5c832ee3d
Toringol <37870973+Toringol@users.noreply.github.com>
2019-06-03 16:50:53 +0300

    Create Division.cpp

---
+++ white_belt/week1/Division.cpp
@@ -0,0 +1,21 @@
+/*
+* Дано два натуральных числа A и B, не превышающих 1 000 000. 
+* Напишите программу, которая вычисляет целую часть частного от деления A на B.
+* Если B = 0, выведите "Impossible".
+*/
+
+#include <iostream>
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	if (b == 0) {
+		std::cout << "Impossible" << std::endl;
+	}
+	else {
+		std::cout << a / b << std::endl;
+	}
+
+	return 0;
+}
