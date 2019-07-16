4c9fd8c06f1dc6860ca6c34c61af77fb6210f13e
Toringol <37870973+Toringol@users.noreply.github.com>
2019-07-16 15:10:40 +0300

    Create startLearner.cpp

---
+++ red_belt/week2/CodeOptimizationPrinciples/LearnNewLanguage/startLearner.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Learner {
+ private:
+  vector<string> dict;
+
+ public:
+  int Learn(const vector<string>& words) {
+    int newWords = 0;
+    for (const auto& word : words) {
+      if (find(dict.begin(), dict.end(), word) == dict.end()) {
+        ++newWords;
+        dict.push_back(word);
+      }
+    }
+    return newWords;
+  }
+
+  vector<string> KnownWords() {
+    sort(dict.begin(), dict.end());
+    dict.erase(unique(dict.begin(), dict.end()), dict.end());
+    return dict;
+  }
+};
+
+int main() {
+  Learner learner;
+  string line;
+  while (getline(cin, line)) {
+    vector<string> words;
+    stringstream ss(line);
+    string word;
+    while (ss >> word) {
+      words.push_back(word);
+    }
+    cout << learner.Learn(words) << "\n";
+  }
+  cout << "=== known words ===\n";
+  for (auto word : learner.KnownWords()) {
+    cout << word << "\n";
+  }
+}
